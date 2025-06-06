289a5e150655eabc219645a71c0140a58c82d6e2
Marlon <sangalangmarlon19@gmail.com>
2025-06-06 20:56:43 +0800

    Add project files.

---
+++ DataAccess/DBContexts/ProjectSuiteDB/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.DBContexts.ProjectSuiteDB.Models;
+
+[Table("User")]
+public partial class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [StringLength(100)]
+    public string? EmployeeName { get; set; }
+
+    [StringLength(100)]
+    public string? Email { get; set; }
+}
