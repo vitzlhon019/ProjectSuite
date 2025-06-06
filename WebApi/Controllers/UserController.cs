9234df7bfe06f3fe914f3c4668cbdf7463d3df26
Marlon <sangalangmarlon19@gmail.com>
2025-06-06 22:31:01 +0800

    Added Initial User Controller, configure middleware

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using Common.DTO;
+using DataAccess.Services.Interfaces;
+using DataAccess.UnitOfWorks._Base;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IBaseUnitOfWork _baseUnitOfWork;
+        private readonly IUserService _userService;
+        public UserController(IUserService userService, IBaseUnitOfWork baseUnitOfWork) {
+            _baseUnitOfWork = baseUnitOfWork;
+            _userService = userService;
+        }
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetAllUsers()
+        {
+
+            var employeeDTOs = await _userService.GetAllEmployeesAsync();
+
+            return Ok(employeeDTOs);
+        }
+        [HttpPost]
+        [Route("AddUser")]
+        public async Task<ActionResult> AddUser([FromBody] UserDTO employeeDto)
+        {
+            if (employeeDto == null)
+                return BadRequest("Invalid User data.");
+
+            await _userService.AddEmployeeAsync(employeeDto);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("GetUser/{id}")]
+        public async Task<IActionResult> GetUser([FromRoute] int id)
+        {
+            var userDto = await _userService.GetEmployeeDetailsAsync(id);
+
+            if (userDto == null)
+                return NotFound();
+
+            return Ok(userDto);
+        }
+
+        [HttpPut]
+        [Route("UpdateUser/{id}")]
+        public async Task<ActionResult> UpdateUser(int id, [FromBody] UserDTO updateEmployeeDTO)
+        {
+            if (updateEmployeeDTO == null)
+            {
+                return BadRequest("User data is required.");
+            }
+
+            var updated = await _userService.UpdateEmployeeAsync(id, updateEmployeeDTO);
+
+            if (!updated)
+            {
+                return NotFound($"User with ID {id} not found.");
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("DeleteUser/{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            var deleted = await _userService.DeleteEmployeeAsync(id);
+
+            if (!deleted)
+            {
+                return NotFound($"User with ID {id} not found.");
+            }
+
+            return NoContent();
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,13 @@
 
+using DataAccess.DBContexts.ProjectSuiteDB;
+using DataAccess.Repositories.ProjectSuite;
+using DataAccess.Repositories.ProjectSuite.Interfaces;
+using DataAccess.Services;
+using DataAccess.Services.Interfaces;
+using DataAccess.UnitOfWorks._Base;
+using DataAccess.UnitOfWorks.ProjectSuiteDB;
+using Microsoft.EntityFrameworkCore;
+
 namespace WebApi
 {
     public class Program
@@ -7,7 +16,25 @@ namespace WebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            //var connectionString = Environment.GetEnvironmentVariable("ProjectSuiteDB_ConnectionString");
+
+            //var connectionString = builder.Configuration["ConnectionStrings:ProjectSuiteDB"];
+            var connectionString = Environment.GetEnvironmentVariable("ProjectSuiteDB_ConnectionString");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("The connection string 'ProjectSuiteDB' was not found in the environment variables.");
+            }
+
+            builder.Services.AddDbContext<ProjectSuiteDBContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddScoped<IBaseUnitOfWork, ProjectSuiteUnitofWork>();
+            builder.Services.AddScoped<IProjectSuiteUnitofWork, ProjectSuiteUnitofWork>();
+
             // Add services to the container.
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IUserService, UserService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
